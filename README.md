c852698d8953844f3bdf7367e6954477fe7e8642
kobaken <kentafly88@gmail.com>
2023-05-16 21:59:20 +0900

    minil new

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# NAME
+
+App::Prove::Plugin::ValuePool - It's new $module
+
+# SYNOPSIS
+
+    use App::Prove::Plugin::ValuePool;
+
+# DESCRIPTION
+
+App::Prove::Plugin::ValuePool is ...
+
+# LICENSE
+
+Copyright (C) kobaken.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+kobaken <kentafly88@gmail.com>
---
+++ lib/App/Prove/Plugin/ValuePool.pm
@@ -0,0 +1,39 @@
+package App::Prove::Plugin::ValuePool;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::Prove::Plugin::ValuePool - It's new $module
+
+=head1 SYNOPSIS
+
+    use App::Prove::Plugin::ValuePool;
+
+=head1 DESCRIPTION
+
+App::Prove::Plugin::ValuePool is ...
+
+=head1 LICENSE
+
+Copyright (C) kobaken.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+kobaken E<lt>kentafly88@gmail.comE<gt>
+
+=cut
+
